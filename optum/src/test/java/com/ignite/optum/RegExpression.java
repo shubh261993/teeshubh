0637286ba44abdfacbacece38f4ef367eaefca28
shubh261993 <shubhammittal440@gmail.com>
2025-03-21 21:51:37 +0530

    update files

---
+++ optum/src/test/java/com/ignite/optum/RegExpression.java
@@ -0,0 +1,25 @@
+package com.ignite.optum;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.testng.annotations.Test;
+
+public class RegExpression {
+	
+	@Test()
+	public void regrx()
+	{
+		Pattern pat = Pattern.compile("e{1}*H+");
+		Matcher match = pat.matcher("Hello wwworlda");
+		boolean find = match.find();
+		if(find)
+		{
+			System.out.println("Pattern Matched");
+		} else {
+			System.out.println("Pattern Not  Matched");
+		}
+		
+	}
+
+}
